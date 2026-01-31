7a744e556e23e2bf83c7b608bed0b5fac1b81e46
basvantilburg159-droid <basvantilburg159@gmail.com>
2026-01-31 13:35:09 +0100

    Add Firebase config

---
+++ firebase-config.js
@@ -0,0 +1,20 @@
+// Fill in your Firebase web app config here.
+// Example:
+// window.FIREBASE_CONFIG = {
+//   apiKey: "...",
+//   authDomain: "...",
+//   projectId: "...",
+//   storageBucket: "...",
+//   messagingSenderId: "...",
+//   appId: "..."
+// };
+
+window.FIREBASE_CONFIG = {
+	apiKey: "AIzaSyAG1hd-L2OU-4pDK1xk_0GAEI5x3Q89LVY",
+	authDomain: "markers-by-bas.firebaseapp.com",
+	projectId: "markers-by-bas",
+	storageBucket: "markers-by-bas.firebasestorage.app",
+	messagingSenderId: "767941633047",
+	appId: "1:767941633047:web:2e0ce8431e8e2a46ade852",
+	measurementId: "G-3Z1YZLS1S4"
+};
